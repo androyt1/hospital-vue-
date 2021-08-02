2533770f80be84418c8964098740437dcb2d2ff9
Andrew Aghoghovwia <androyt1@gmail.com>
2021-08-02 22:35:15 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">    
+    <Androy greeting="I am testing out vue js once again"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Androy from '@/components/Androy.vue'
+
+export default {
+  name: 'Home',
+  components: {   
+    Androy
+  }
+}
+</script>
